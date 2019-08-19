f4f973a628663b1d2882f7208a1f222fbef5421e
Alex Mendes <mendes.develop@gmail.com>
2019-08-19 20:00:22 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,16 +14,6 @@ def third_challenge
         :age => "15"}
         },
       :hero_friends => [
-        {
-        :name => "Benvolio",
-        :age => "17",
-        :attitude => "worried"
-      },
-      {
-        :name => "Mercutio",
-        :age => "18",
-        :attitude => "hot-headed"
-      }
         ]
    }, 
    :capulet => {
@@ -40,7 +30,19 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        
+        {
+        :name => "Steven",
+        :age => "30",
+        :attitude => "confused"
+        },
+        {
+        :name => "Nurse",
+        :age => "44",
+        :attitude => "worried"
+        }
+        ]
    }
   }
 
