098a75cb0f8aedd6af7fe179078ab298cba6bc36
Alex Mendes <mendes.develop@gmail.com>
2019-08-19 19:56:54 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,8 +6,7 @@ def second_challenge
         
       },
       :matriarch => {
-        :name => "Lady Montague",
-        :age => "54"
+        
       },
       :hero => {:name => "Romeo",
         :age => "15"}
