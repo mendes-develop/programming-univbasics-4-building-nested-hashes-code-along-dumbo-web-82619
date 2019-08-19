361f13c5470967c5fa695ecd0b27fce0468ad6ea
Alex Mendes <mendes.develop@gmail.com>
2019-08-19 20:08:16 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,16 +6,16 @@ def second_challenge
       :matriarch => {},
       :hero => {}
       :hero_friends => []
-    }, 
+    },
+    
     :capulet => 
     {
-      
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
-      
     }
+  }
 
   
 end
