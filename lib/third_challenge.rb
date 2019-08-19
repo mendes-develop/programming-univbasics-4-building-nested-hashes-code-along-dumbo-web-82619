6df42ba316020f1ad9fcfd538a199533bb33a7e7
Alex Mendes <mendes.develop@gmail.com>
2019-08-19 20:14:53 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,8 +13,7 @@ def third_challenge
         :name => "Romeo",
         :age => "15"}
         },
-      :hero_friends => [
-        ]
+      :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
@@ -23,15 +22,14 @@ def third_challenge
         },
       :matriarch => {
         :name => "Lady Capulet",
-        :age => "51"},
+        :age => "51"
         },
       :heroine => {
         :name => "Juliet",
         :age => "15",
         :status => "alive"
         },
-      :heroine_friends => [
-        ]
+      :heroine_friends => []
    }
   }
 
