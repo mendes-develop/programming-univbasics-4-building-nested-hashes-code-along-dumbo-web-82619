e233e6dcb767d52f86caa33090500dfbbf658187
Alex Mendes <mendes.develop@gmail.com>
2019-08-19 20:06:22 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,27 +1,18 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      :patriarch => {
-        
-      },
-      :matriarch => {
-        
-      },
-      :hero => {
-        
+    :montague => 
+    {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {}
       :hero_friends => []
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      :patriarch => {
-        
-      },
-      :matriarch => {
-        
-      :heroine => {
-        
-      },
+    }, 
+    :capulet => 
+    {
+      
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
       :heroine_friends => []
       
     }
