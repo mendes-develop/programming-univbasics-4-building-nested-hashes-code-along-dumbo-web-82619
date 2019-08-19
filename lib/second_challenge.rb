e17554473484ab0b12cb80b74072b828a1caff0c
Alex Mendes <mendes.develop@gmail.com>
2019-08-19 19:55:03 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,9 +2,48 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
+      :patriarch => {
+        :name => "Lord Montague",
+        :age => "53"
+      },
+      :matriarch => {
+        :name => "Lady Montague",
+        :age => "54"
+      },
+      :hero => {:name => "Romeo",
+        :age => "15"}
+      :hero_friends => [
+        {
+        :name => "Benvolio",
+        :age => "17",
+        :attitude => "worried"
+      },
+      {
+        :name => "Mercutio",
+        :age => "18",
+        :attitude => "hot-headed"
+      }
+        ]
       }, 
     :capulet => {
       #your key/value pairs here!
+      :patriarch => {
+        :name => "Lord Capulet",
+        :age => "50"
+      },
+      :matriarch => {
+        :name => "Lady Capulet",
+        :age => "51"},
+      :heroine => {
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
+      },
+      :heroine_friends => [
+        {
+          
+        }
+        ]
       }}
 
   
